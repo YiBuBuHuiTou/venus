1a2111d36f86be238136f387bef03df3b3006f72
cxd <591018214@qq.com>
2021-03-05 19:19:39 +0800

    no message

--- auth/src/main/java/com/cxd/venus/auth/AuthApplication.java
+++ auth/src/main/java/com/cxd/venus/auth/AuthApplication.java
@@ -1,8 +1,18 @@
 package com.cxd.venus.auth;
 
+/**
+ * @Author YiBuBuHuiTou
+ * @Description
+ * @Date 2021/2/28 19:51
+ * @Version 1.0
+ **/
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+//开启缓存
+@EnableCaching
 @SpringBootApplication
 public class AuthApplication {
 
---
+++ auth/src/main/java/com/cxd/venus/auth/config/OAuth2Config.java
@@ -0,0 +1,14 @@
+package com.cxd.venus.auth.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author YiBuBuHuiTou
+ * @Description
+ * @Date 2021/2/28 19:51
+ * @Version 1.0
+ **/
+@Configuration
+public class OAuth2Config {
+
+}
